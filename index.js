8cc0b226994312e1ba05ad0ca0c9c7c47d6946d6
Hleb Muravitski <h.muravitski@godeltech.com>
2024-12-11 18:04:53 +0100

    solve task 5

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-require('dotenv').config();
-const express = require('express')
-const axios = require('axios');
-const app = express()
-const { link, token, port } = require('./config/config');
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
+const app = express();
+const { link, token, port } = require("./config/config");
+let resp ;
 
-axios
-  .get(link + token)
-  .then((response) => {
-    console.log("Response:", response.data.near_earth_objects['2024-12-11'][0].name);
-})
+axios.get(link + token).then((response) => {
+  resp= response.data;
+});
 
-  app.listen(port||4000,()=>{
-    console.log('Server is running on port: ' + port||4000);
-   })
+app.get("/meteors", (req, res) => {
+  res.send(resp);
+});
+
+app.listen(port || 4000, () => {
+  console.log("Server is running on port: " + port || 4000);
+});
