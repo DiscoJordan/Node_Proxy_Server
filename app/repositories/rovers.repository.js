226232dda0dd61fc88520e2fe8fa3557143603c7
Hleb Muravitski <h.muravitski@godeltech.com>
2024-12-16 15:19:00 +0100

    remove trycatch section from rovers repository

--- app/repositories/rovers.repository.js
+++ app/repositories/rovers.repository.js
@@ -3,19 +3,16 @@ import { config } from "../config/config.js";
 
 export default async function fetchImages() {
   const { API_KEY } = config;
-  try {
-    const response = await axios.get(
-      "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos",
-      {
-        params: {
-          sol: 1000,
-          camera: "fhaz",
-          api_key: API_KEY,
-        },
-      }
-    );
-    return response.data.photos;
-  } catch (error) {    
-    throw new Error("Failed to fetch Images from NASA API");
-  }
+
+  const response = await axios.get(
+    "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos",
+    {
+      params: {
+        sol: 1000,
+        camera: "fhaz",
+        api_key: API_KEY,
+      },
+    }
+  );
+  return response.data.photos;
 }
