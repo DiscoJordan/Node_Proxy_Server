6ce144b4dd2594839e438b4eb9c29106fac3022c
Hleb Muravitski <h.muravitski@godeltech.com>
2024-12-22 15:01:41 +0100

    task 15 update ts config

--- app/utils/errorHandler.js
+++
@@ -1,5 +0,0 @@
-export default function errorHandler(err, req, res) {
-    const status = err.statusCode || 500;
-    const message = err.message || "Internal Server Error";
-    res.status(status).json({ error: message });
-}
