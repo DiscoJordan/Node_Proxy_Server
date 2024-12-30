67830a9b6b4a0f267b3ae36d2718ce9752ab6a26
Hleb Muravitski <h.muravitski@godeltech.com>
2024-12-30 14:58:43 +0100

    update config

--- instrument.js
+++ instrument.js
@@ -1,8 +1,9 @@
 import { init } from "@sentry/node";
 import { nodeProfilingIntegration } from "@sentry/profiling-node";
+import { config } from "./app/config/config.js";
 
 init({
-  dsn: process.env.DSN,
+  dsn: config.DSN,
   integrations: [nodeProfilingIntegration()],
   // Tracing
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
